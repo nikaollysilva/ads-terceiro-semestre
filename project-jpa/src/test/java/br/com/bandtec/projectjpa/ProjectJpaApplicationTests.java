9b33d03be3a0c40c5271c04a8919ec015f191511
Nikolly Silva <53615322+nikaollysilva@users.noreply.github.com>
2020-09-25 23:58:52 -0300

    add conexao jpa e cruds em spring boot

---
+++ project-jpa/src/test/java/br/com/bandtec/projectjpa/ProjectJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bandtec.projectjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
